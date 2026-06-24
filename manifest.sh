4f70b2be6970b6724a9d9a06116b693cd94fa5b6
Jason Lethbridge <lethbridgejason@gmail.com>
2026-06-24 10:52:03 +1000

    Updated LTS releases

--- manifest.sh
+++ manifest.sh
@@ -20,11 +20,11 @@ https://download.blender.org/release/Blender3.3/blender-3.3.21-linux-x64.tar.xz"
 manifest[5]="https://download.blender.org/release/Blender3.6/blender-3.6.23.sha256 \
 https://download.blender.org/release/Blender3.6/blender-3.6.23-linux-x64.tar.xz"
 
-manifest[6]="https://download.blender.org/release/Blender4.2/blender-4.2.21.sha256 \
-https://download.blender.org/release/Blender4.2/blender-4.2.21-linux-x64.tar.xz"
+manifest[6]="https://download.blender.org/release/Blender4.2/blender-4.2.22.sha256 \
+https://download.blender.org/release/Blender4.2/blender-4.2.22-linux-x64.tar.xz"
 
-manifest[7]="https://download.blender.org/release/Blender4.5/blender-4.5.10.sha256 \
-https://download.blender.org/release/Blender4.5/blender-4.5.10-linux-x64.tar.xz"
+manifest[7]="https://download.blender.org/release/Blender4.5/blender-4.5.11.sha256 \
+https://download.blender.org/release/Blender4.5/blender-4.5.11-linux-x64.tar.xz"
 
 manifest[8]="https://download.blender.org/release/Blender5.1/blender-5.1.2.sha256 \
 https://download.blender.org/release/Blender5.1/blender-5.1.2-linux-x64.tar.xz"
